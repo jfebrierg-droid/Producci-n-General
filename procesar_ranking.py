7d1bcecbeecbe54246bd1d12a409320a5f33fbc3
jfebrierg-droid <jfebrierg@gmail.com>
2026-09-04 09:19:04 -0400

    Update procesar_ranking.py

--- procesar_ranking.py
+++ procesar_ranking.py
@@ -9,7 +9,7 @@ def procesar_y_enviar():
 
     # Credenciales de envío (si no usas GitHub Secrets, pon tus datos aquí)
     sender_email = os.environ.get("EMAIL_USER", "jfebrierg@gmail.com")
-    password = os.environ.get("EMAIL_PASSWORD", "COntrace120675")
+    password = os.environ.get("EMAIL_PASSWORD", "COntrace120678")
     recipient_email = os.environ.get("EMAIL_RECIPIENT", "jfebrierg@gmail.com")
 
     # Diagnóstico para verificar la carga de credenciales
